b10b05e1339f2e7951f2718d2d236d2cd7e96a6b
jcozain <jesus.cozain@gmail.com>
2017-01-16 06:33:30 -0600

    working ajax polygons

--- mapa/views.py
+++ mapa/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.db import connection
+from django.http import HttpResponse
+import json
 
 
 def index(request):
     return render(request,'mapa/mapa.html')
+
+def get_manzanas(request):
+	radio=request.GET.get('radio',"100")
+	lat=request.GET.get('lat',"19.4")
+	lng=request.GET.get('long',"-99.1")
+	with connection.cursor() as cursor:
+	 	cursor.execute("select manzanasporradio("+lng+","+lat+","+radio+")")
+	 	results=cursor.fetchone()
+	 	data= json.dumps(results[0])
+	return HttpResponse(data, content_type='application/json')
+	
